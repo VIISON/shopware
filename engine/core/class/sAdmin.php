29d7b6ca10df787f0283455e0ce5806820674a24
Samuel Vogel <samuel.vogel@viison.com>
2013-11-29 17:13:33 +0000

    Don't round shipping discounts/surcharges to full numbers

--- engine/core/class/sAdmin.php
+++ engine/core/class/sAdmin.php
@@ -4770,7 +4770,7 @@ class sAdmin
             } elseif ($dispatch['calculation'] == 2) {
                 $from = round($basket['count_article']);
             } elseif ($dispatch['calculation'] == 3) {
-                $from = round($basket['calculation_value_' . $dispatch['id']]);
+                $from = round($basket['calculation_value_' . $dispatch['id']], 2);
             } else {
                 continue;
             }
