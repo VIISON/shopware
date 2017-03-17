d2e937b6de4e21b10efdbe4950744cce6986c490
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-17 15:59:37 +0100

    Add basket test *** will be deleted later My notebook will be repaired, this is basically a „backup“ =)

--- tests/Functional/Controllers/Frontend/CheckoutTest.php
+++ tests/Functional/Controllers/Frontend/CheckoutTest.php
@@ -85,4 +85,112 @@ class Shopware_Tests_Controllers_Frontend_CheckoutTest extends Enlight_Component
         $this->dispatch('/checkout/addArticle/sAdd/'.self::ARTICLE_NUMBER);
         return Shopware()->Container()->get('SessionID');
     }
+
+    public function loginFrontendUser()
+    {
+        Shopware()->Front()->setRequest(new Enlight_Controller_Request_RequestHttp());
+        $user = Shopware()->Container()->get('dbal_connection')->fetchAssoc(
+            'SELECT id, email, password, subshopID, language FROM s_user WHERE id = :userId',
+            [
+                ':userId' => 1
+            ]
+        );
+
+        /** @var $repository Shopware\Models\Shop\Repository */
+        $repository = Shopware()->Models()->getRepository('Shopware\Models\Shop\Shop');
+        $shop = $repository->getActiveById($user['language']);
+
+        $shop->registerResources();
+
+        Shopware()->Session()->Admin = true;
+        Shopware()->System()->_POST = array(
+            'email' => $user['email'],
+            'passwordMD5' => $user['password'],
+        );
+        Shopware()->Modules()->Admin()->sLogin(true);
+    }
+
+    private function addBasketArticleWithQuantity($quantity = 1)
+    {
+        $this->reset();
+        $this->Request()->setHeader('User-Agent', self::USER_AGENT);
+        $this->Request()->setParam('sQuantity', $quantity);
+        $this->dispatch('/checkout/addArticle/sAdd/'.self::ARTICLE_NUMBER);
+        return Shopware()->Container()->get('SessionID');
+    }
+
+    /**
+     * @group dev_thomas
+     */
+    public function testCheckoutForNetOrders() {
+        $net = true;
+        $this->runCheckoutTest($net);
+        for($i=19;$i<30; $i++) {
+            $this->runCheckoutTest($net, $i);
+        }
+    }
+
+    /**
+     * @group dev_thomas
+     */
+    public function testCheckoutForGrossOrders() {
+        $net = false;
+        $this->runCheckoutTest($net);
+
+    }
+
+    public function runCheckoutTest($net = false, $quantity=1) {
+        $this->loginFrontendUser();
+
+        $tax = $net == true ? 0 : 1;
+
+        // Set net customer group
+        $defaultShop = Shopware()->Models()->getRepository(\Shopware\Models\Shop\Shop::class)->find(1);
+        $previousCustomerGroup = $defaultShop->getCustomerGroup()->getKey();
+        $netCustomerGroup = Shopware()->Models()->getRepository(\Shopware\Models\Customer\Group::class)->findOneBy(['tax' => $tax])->getKey();
+        $this->assertNotEmpty($netCustomerGroup);
+        Shopware()->Db()->query(
+            "UPDATE s_user SET customergroup = ? WHERE id = 1",
+            array($netCustomerGroup)
+        );
+
+        // Add article to basket
+        $this->addBasketArticleWithQuantity($quantity);
+
+        // Confirm checkout
+        $this->reset();
+        $this->Request()->setHeader('User-Agent', self::USER_AGENT);
+        $this->dispatch('/checkout/confirm');
+
+        // Finish checkout
+        $this->reset();
+        $this->Request()->setHeader('User-Agent', self::USER_AGENT);
+        $this->Request()->setParam('sAGB', 'on');
+        $this->dispatch('/checkout/finish');
+
+        // Revert customer group
+        Shopware()->Db()->query(
+            "UPDATE s_user SET customergroup = ? WHERE id = 1",
+            array($previousCustomerGroup)
+        );
+
+        // Fetch created order
+        $orderId = Shopware()->Db()->fetchOne(
+            "SELECT id FROM s_order ORDER BY ID DESC LIMIT 1"
+        );
+        /** @var \Shopware\Models\Order\Order $order */
+        $order = Shopware()->Models()->getRepository(\Shopware\Models\Order\Order::class)->find($orderId);
+
+        $previousInvoiceAmount = $order->getInvoiceAmount();
+        $previousInvoiceAmountNet = $order->getInvoiceAmountNet();
+
+        // Simulate backend order save
+        $order->calculateInvoiceAmount();
+
+        // Test that sBasket calculation matches calculateInvoiceAmount
+        $this->assertEquals(round($order->getInvoiceAmount(), 2), round($previousInvoiceAmount, 2), 'InvoiceAmount');
+        $this->assertEquals(round($order->getInvoiceAmountNet(), 2), round($previousInvoiceAmountNet, 2), 'InvoiceAmountnet');
+
+        Shopware()->Modules()->Admin()->logout();
+    }
 }
